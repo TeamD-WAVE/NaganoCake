8252925577dc23352fb4a4ed6e1d65aaad433b6c
EC2 Default User <ec2-user@ip-172-31-11-247.ap-northeast-1.compute.internal>
2024-01-22 03:24:25 +0000

    Add address

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,8 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def addresses
+    postcode + address + name
+  end
 end
