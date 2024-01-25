2d8393e1448e72883e4f2d3bfa375f2aed709b52
uraokaathushi <varon.918@gmail.com>
2024-01-25 18:12:58 +0000

    会員一覧画面作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,15 +1,15 @@
  class Admin::CustomersController < ApplicationController
-  validates :is_active, inclusion: {in: [true, false]}  
+
   def index
-   @customers = Public::Customer.all.page(params[:page]).per(3)
-  end 
-    
+   @customers = Customer.all.page(params[:page]).per(3)
+  end
+
   def show
-   @customer = Public::Customer.find(params[:id])
-  end 
-  
+   @customer = Customer.find(params[:id])
+  end
+
   def edit
-   @customer = Public::Customer.find(params[:id])
+   @customer = Customer.find(params[:id])
   end
 
   def update
@@ -17,11 +17,11 @@
    @customer.update(customer_params)
    redirect_to admin_customer_path
   end
-    
+
   private
 
   def customer_params
     params.require(:customer).permit(:email, :encrypted_password, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
   end
-    
+
  end
