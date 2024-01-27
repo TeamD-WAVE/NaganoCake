386075ff27bfde8200905b1bcfa6cae91fb1e382
EC2 Default User <ec2-user@ip-172-31-43-93.ap-northeast-1.compute.internal>
2024-01-27 11:15:53 +0000

    商品作成時画像必須

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,5 +11,7 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :introduction, presence: true
   validates :price, presence: true
+  validates :image, presence: true
+  
 end
 
